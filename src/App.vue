dcef90244f3f2cfc4c469e24cf36dbfae02f893f
liuyulong <liuyulong@syswin.com>
2017-08-30 17:57:30 +0800

    vuex project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h2>这是由多个组件修改一个state的例子，可以看到所有组件的state一同变化，而且每个组件中都可以操作state的变化。</h2>
+    <hr>
+    <hello></hello>
+    <!-- <single></single> -->
+    <ma></ma>
+    <mb></mb>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Single from './components/Single'
+import ma from './components/A'
+import mb from './components/B'
+
+export default {
+  name: 'app',
+  components: {
+    ma,
+    mb,
+    Hello,
+    Single,
+  }
+}
+</script>
---
+++ src/components/A.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    多页面显示同一个state数据： {{ $store.state.count }}
+    <button @click="increment">同步+1</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      increment () {
+        // 使用mutations同步修改state
+        this.$store.commit('increment')
+      }
+    }
+  }
+</script>
